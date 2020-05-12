3d80c223c53a8a6110ea31c752d8a093daa01592
Alessandro <alecritelli@gmail.com>
2020-05-13 00:34:03 +0200

    bug fixing

--- ex.2/main.js
+++ ex.2/main.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     return months;
   };
 
-  function ajaxCall(typeOfChart) {
+  function ajaxCall() {
 
     $.ajax({
       url: "server.php",
@@ -16,7 +16,45 @@ $(document).ready(function() {
         console.log(data);
         var ctx = $('#myChart');
         var myChart = new Chart(ctx, {
-            type: typeOfChart,
+            type: "line",
+            data: {
+                labels: getMonthsList(),
+                datasets: [{
+                    label: 'Vendite',
+                    data: data.monthlySales,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+        var ctx = $('#myPieChart');
+        var myChart = new Chart(ctx, {
+            type: "pie",
             data: {
                 labels: data.name,
                 datasets: [{
@@ -41,6 +79,7 @@ $(document).ready(function() {
                     borderWidth: 1
                 }]
             },
+
             options: {
                 scales: {
                     yAxes: [{
@@ -55,10 +94,10 @@ $(document).ready(function() {
     }); // fine della chiamata ajax
   } // fine della funzione ajax call
 
-  function printChart(typeOfChart) {
-    ajaxCall(typeOfChart);
+  function printChart() {
+    ajaxCall();
   } // fine della function che disegna il grafico
 
-  printChart("pie");  // <-- pass the argument in the printChart function to select the type of chart.
+  printChart();  // <-- pass the argument in the printChart function to select the type of chart.
 
 });
--- ex.2/server.php
+++ ex.2/server.php
@@ -6,13 +6,13 @@
 
   $res[] = array_keys($graphs[fatturato_by_agent][data]);
   $res[] = array_values($graphs[fatturato_by_agent][data]);
+  $res[] = array_values($graphs[fatturato][data]);
 
     $res= [
       "name" => $res[0],
-      "value"=> $res[1]
+      "value"=> $res[1],
+      "monthlySales"=> $res[2]
     ];
 
-  // print_r($res);
-  // print_r($abc);
   echo json_encode($res);
  ?>
